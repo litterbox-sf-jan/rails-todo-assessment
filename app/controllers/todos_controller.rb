529f2acc1706b0824ca7d34bf99e5887a5f2bede
Alex Notov <alex@zealoushacker.com>
2015-03-16 09:05:36 -0700

    Updating todos
    Common redirect for both update and create
    Form partial for new and edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  after_action :redirect_to_root, only: [:create, :update]
+
   def index
     @todos = Todo.order(created_at: :desc)
   end
@@ -8,14 +10,18 @@ class TodosController < ApplicationController
 
   def create
     Todo.create todo_params
-    redirect_to root_path
   end
 
   def update
+    Todo.find(params[:id]).update_attributes(todo_params)
   end
 
   def new
     @todo = Todo.new
+  end
+
+  def edit
+    @todo = Todo.find params[:id]
   end
 
   def destroy
@@ -24,5 +30,9 @@ class TodosController < ApplicationController
   private
   def todo_params
     params.require(:todo).permit(:name)
+  end
+
+  def redirect_to_root
+    redirect_to root_path
   end
 end
